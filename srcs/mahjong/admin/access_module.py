4b84017ef8fc15bd27d69265c5ba769e64370b86
lyg <1983654762@qq.com>
2018-09-06 11:03:56 +0800

    m

--- srcs/mahjong/admin/access_module.py
+++ srcs/mahjong/admin/access_module.py
@@ -308,7 +308,8 @@ ACCESS_COMPANY_MODULES = (
     AccessObj(("order", "buy"), 'GET', 'MENU_ORDER_BUY_TXT'), \
     AccessObj(("order", "buy/record"), 'GET', 'MENU_ORDER_BUY_RECORD_TXT'), \
     AccessObj(("order", "sale/record"), 'GET', 'MENU_ORDER_SALE_RECORD_TXT'), \
-
+    AccessObj(("fish", "chargeAndOrder"), 'GET', 'MENU_FISH_CHARGE_AND_ORDERS'), \
+ \
     AccessObj(("accounts"), None, 'MENU_ACCOUNTS_TXT',True), \
     AccessObj(("statistics","saleReport"), 'GET', 'MENU_STATISTICS_SALEREPORT_TXT'), \
     AccessObj(("statistics", "buyReport"), 'GET', 'MENU_STATISTICS_BUYREPORT_TXT'), \
